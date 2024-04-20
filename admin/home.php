24a385e28b7398b739913413a149c6c73a736eca
Renel-Joy <159543239+Renel-Joy@users.noreply.github.com>
2024-04-20 09:32:54 +0800

    Add files via upload

---
+++ admin/home.php
@@ -0,0 +1,33 @@
+
+<div class="dashboard-img">
+    <img src="../profile_asset/images/ccs.png">
+    <h1>Welcome to <?php echo $_settings->info('name') ?></h1>
+    <img id="img2" src="../profile_asset/images/ccsdept.png">
+</div>
+
+
+
+
+<style>
+  
+        
+  
+    .dashboard-img {
+        padding-top: 2rem;
+        display: flex;
+        justify-content: space-between;
+        text-align: center;
+        align-items: center;
+        
+    }
+
+    .dashboard-img h1 {
+        margin-bottom: 5rem;
+        margin-left: 2rem;
+        
+    }
+    #img2 {
+        width: 11rem;
+        height: 11rem;
+    }
+</style>
